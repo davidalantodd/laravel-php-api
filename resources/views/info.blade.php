6e20bb71cad2b410b7113007d3e6aa102d481885
davidalantodd <107488171+davidalantodd@users.noreply.github.com>
2024-11-13 14:52:06 -0500

    add test info view and route

---
+++ resources/views/info.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hehe</title>
+</head>
+<body>
+    <main>
+        <h1>testing this out!</h1>
+    </main>
+    
+</body>
+</html>
